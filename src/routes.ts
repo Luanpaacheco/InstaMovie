b1d2e9747ddfd29245df757a9b0859fe75e1fa6c
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-09-29 02:00:27 -0300

    deixando a api funcional, testei com thunderClient

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from 'fastify';
 import { request } from 'http';
+import { CreateUserController } from './controllers/CreateUserController';
 
 export async function routes(fastify:FastifyInstance, options:FastifyPluginOptions){
     fastify.get("/teste", async (request:FastifyRequest, reply:FastifyReply)=>{
         return {ok:true}
+    })
+
+    fastify.post("/user", async(request:FastifyRequest, reply:FastifyReply)=>{
+        return new CreateUserController().handle(request,reply)
     })
 }
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,26 @@
+import prismaClient from "../prisma";
+
+interface CreateUserProps{
+    name:string;
+    email:string;
+}
+
+class CreateUserService{
+    async execute({name,email}:CreateUserProps){
+        if(!name||!email){
+            throw new Error("preencha todos os campos")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name,
+                email
+            }
+        })
+
+        return user
+    }
+    
+}
+
+export {CreateUserService}
